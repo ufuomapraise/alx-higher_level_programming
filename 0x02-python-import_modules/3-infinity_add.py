f5e02d2ea0c1820924662023c39266b4e0624848
ufuomapraise <ufuomapraise@gmail.com>
2024-03-18 11:57:35 +0100

    praise

---
+++ 0x02-python-import_modules/3-infinity_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+
+    total = 0
+
+    for i in argv[1:]:
+        total += int(i)
+
+    print(" {} ".format(total))
+~                                                               ~                                     
