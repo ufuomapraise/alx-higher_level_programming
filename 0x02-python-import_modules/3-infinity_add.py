2d94c96e15542065dcd2f1dc89dc9fd8b1fcf4cd
ufuomapraise <ufuomapraise@gmail.com>
2024-03-18 12:00:20 +0100

    addition of numbers on command line using python

--- 0x02-python-import_modules/3-infinity_add.py
+++ 0x02-python-import_modules/3-infinity_add.py
@@ -9,4 +9,3 @@ if __name__ == "__main__":
         total += int(i)
 
     print(" {} ".format(total))
-~                                                               ~                                     
